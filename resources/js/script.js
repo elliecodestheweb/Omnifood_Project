aca197f8db40fb644d7bf9d8bf66907b8844b3ac
Ellie <elliecodestheweb@gmail.com>
2018-12-05 19:12:55 -0800

    finished sticky nav and link scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,49 @@
 $(document).ready(function() {
+  
+    /* Sticky navigation */
+
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky')
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
     
+    /* Scroll on link click */
 
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
 
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    });
+
+    /* Navigation scroll */
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    }); 
+
+    /*
+    var waypoints = $('handler-first').waypoint(function(direction)) {
+        notify(this.element.id + ' hit 25% from top of window')
+    }, {
+        offset:'25%'
+    })
+    */
 });
